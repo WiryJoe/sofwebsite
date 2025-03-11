346d71624a800bf14e3a6c5b816c71710747b284
Wiry Joe <rhyder1981@gmail.com>
2025-03-11 17:21:33 -0600

    Made a pointer for the FAQ section to point to the active index to make it a little clearer for the user where to look.

--- src/Components/FAQSection.js
+++ src/Components/FAQSection.js
@@ -55,7 +55,26 @@ const FAQSection = () => {
           <div
             className={`mt-4 text-white ${activeIndex === index ? "block" : "hidden"}`}
           >
-            <p className="text-left">{faq.answer}</p>
+            <div className="relative">
+              <p className="text-left">{faq.answer}</p>
+
+              <div className="absolute -bottom-4 -left-12 flex items-center w-12 text-center p-4 rounded-full">
+                <svg
+                  className="h-5 w-5 rotate-90 text-red-600"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="6 6 12 10"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="4"
+                    d="M5 15l7-7 7 7"
+                  ></path>
+                </svg>
+              </div>
+            </div>
           </div>
         </div>
       ))}
